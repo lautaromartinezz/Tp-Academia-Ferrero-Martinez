1fbca5f8860ee97a173f95b6e6f866f86a2f8c93
Lautaro Martinez <lautimartinez.2016@gmail.com>
2025-10-25 15:30:53 -0300

    add blazor usuario plan materia y curso

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -12,6 +12,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHttpLogging(o => { });
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.MapMateriaEndpoints();
 app.MapCursoEndpoints();
