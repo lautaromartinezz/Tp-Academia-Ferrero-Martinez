2bae0b8c0e4862b6c4cb7c7f46bc8fe2d223ee25
Santiago Ferrero <santifnob@gmail.com>
2025-11-17 01:35:37 -0300

    Add first version of cursos report

--- WebAPI/Reports/CursosReport.cs
+++ WebAPI/Reports/CursosReport.cs
@@ -5,6 +5,10 @@ namespace WebAPI.Reports
 {
     public class CursosReport : XtraReport
     {
+        private TopMarginBand topMarginBand1;
+        private DetailBand detailBand1;
+        private BottomMarginBand bottomMarginBand1;
+
         public CursosReport(IEnumerable<CursoDTO> data)
         {
             DataSource = data;
@@ -18,6 +22,36 @@ namespace WebAPI.Reports
             };
 
             detail.Controls.Add(label);
+        }
+
+        private void InitializeComponent()
+        {
+            this.topMarginBand1 = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.detailBand1 = new DevExpress.XtraReports.UI.DetailBand();
+            this.bottomMarginBand1 = new DevExpress.XtraReports.UI.BottomMarginBand();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // topMarginBand1
+            // 
+            this.topMarginBand1.Name = "topMarginBand1";
+            // 
+            // detailBand1
+            // 
+            this.detailBand1.Name = "detailBand1";
+            // 
+            // bottomMarginBand1
+            // 
+            this.bottomMarginBand1.Name = "bottomMarginBand1";
+            // 
+            // CursosReport
+            // 
+            this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
+            this.topMarginBand1,
+            this.detailBand1,
+            this.bottomMarginBand1});
+            this.Version = "25.1";
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
         }
     }
 }
---
+++ WebAPI/Reports/XtraReport1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+using DevExpress.XtraReports.UI;
+
+namespace WebAPI.Reports
+{
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport
+    {
+        public int idUsuario; // El que llega de afuera es el id del usuario
+        public XtraReport1(int idUsuarioDado)
+        {
+            idUsuario = idUsuarioDado;
+            InitializeComponent();
+            // Parameters["IdPersona"].Value = 1;
+        }
+    }
+}
